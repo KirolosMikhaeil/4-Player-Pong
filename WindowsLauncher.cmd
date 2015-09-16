79699e7cdc1eebad4134467b2a9371e6ba271833
CoderEagel <sidney.bruneder854@outlook.com>
2015-09-16 13:44:32 +0930

    Basic launcher for windows users with no links to the python enviroment

---
+++ WindowsLauncher.cmd
@@ -0,0 +1,16 @@
+@echo off
+color 0a
+title 4 PlayerPong
+echo |----------------------------------|
+echo | Batch launcher for 4 player P!ng |
+echo |----------------------------------|
+echo Made by CoderEagel, read LICENSE and README for details
+echo.
+echo ------------------------------------
+echo.
+echo Executing with no arguments ->
+echo Python Main.py
+echo.
+Python Main.py
+echo Finished.
+exit
