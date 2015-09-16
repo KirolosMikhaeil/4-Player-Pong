fcc02cf3ee14693e8538020a3e1dc84841101c7f
CoderEagel <sidney.bruneder854@outlook.com>
2015-09-16 13:37:20 +0930

    Added test Ai and Collision Logic, Controls glitch

--- Main.py
+++ Main.py
@@ -8,34 +8,61 @@ def Ai():
         bool = True
     return bool #Chance of faultering
 
+def AiMatch(axisY, Aipos, PingX, PingY):
+   #    MoveAxis | AiPlayer Position [percent] | PingPong Position
+    if axisY:
+        if Aipos < PingY and Ai():
+            Aipos += MOVESPEED
+        if Aipos > PingY and Ai():
+            Aipos -= MOVESPEED
+    else:
+            if Aipos < PingX and Ai():
+                Aipos += MOVESPEED
+            if Aipos > PingX and Ai():
+                Aipos -= MOVESPEED
+
+    return Aipos
+
 def Render():
     pygame.display.update()
 
 def Init():
     pygame.init()
     pygame.display.set_caption('P!ng')
-    pygame.mouse.set_visible(False)
+    #pygame.mouse.set_visible(False)
+    
+def Logic(a, b):
+    return a, b
    
-def Colis():
-    return None
+def Colis(arect, brect, pingxdir, pongydir):   #COLLSION OBJECT
+    #on collsion
+    returnArray[0-pingxdir, 0-pongydir] #invert Direction? 
+    return returnArray
 
 Init() #Init Window
 
 Score = [0,0,0,0] #ScoreBoard Data
 PlayerPosition= [50,50,50,50] #PLayer Padels
+PlayerLives= [3,3,3,3]
 Moving = [0, 0, 0, 0] # 0=stationary, 1=UP, 2=down
 
 White = (255,255,255)
 Black = (0,0,0)
+Player1CL = White
+Player2CL = White
+Player3CL = White
+Player4CL = White
 
 Width = 1280 #Screen
 Height = 620
 Boundary = 360 #Margin
 FPS = 40
 
+MOVESPEED = 0.2
+
 PingPong = [375, 375] #Ball
-PingPongSpeed = 0 #Ball Speed
-PingPongDirection = 1 #Ball Direction
+PingPongSpeed = 1 #Ball Speed
+PingPongDirection = [0, 0] #Ball Direction [x, ydirection]
 GamePlaying = True #QuitEventArg
 PTemp = PlayerPosition
 PMove = Moving
@@ -45,13 +72,12 @@ windowSurface = pygame.display.set_mode((Width, Height))
  
 
 while GamePlaying: #Loop
-       #Logic here
-      pygame.draw.rect(windowSurface, Black, (0,0,Width,Height)) #Clear Screen
+      
+      #Logic Calls
 
-      pygame.draw.rect(windowSurface, White, (Boundary, ((Height * (PlayerPosition[0] / 100))-20), 5, 40)) #Player One
-      pygame.draw.rect(windowSurface, White, (Width-Boundary-5, ((Height * (PlayerPosition[1] / 100))-20), 5, 40)) #Player Two
-      pygame.draw.rect(windowSurface, White, (((Width * (PlayerPosition[2] / 100))-20), 0, 40, 5)) #Player Three
-      pygame.draw.rect(windowSurface, White, (((Width * (PlayerPosition[3] / 100))-20), Height-5, 40, 5)) #Player Four
+      # Ai
+      PTemp[3] = AiMatch(False, PTemp[3], PingPong[0], PingPong[1])
+      #Input Call----------------------------------------------------------------
       for event in pygame.event.get():
           if event.type == pygame.KEYUP: 
             if event.key == K_s:
@@ -73,12 +99,22 @@ while GamePlaying: #Loop
                 PTemp[1] -= 1
           if event.type == QUIT:
             GamePlaying = False
-
+      PTemp[3]=pygame.mouse.get_rel()[0]
       for i in PMove:
             if PMove[i] == 1:
-                PTemp[i] -= 1
+                PTemp[i] -= MOVESPEED
             if PMove[i] == 2:
-                PTemp[i] += 1
+                PTemp[i] += MOVESPEED
+
+      #Draw Calls -------------------------------------------------------------------------------
+      pygame.draw.rect(windowSurface, Black, (Boundary,0,Width-(2*Boundary),Height)) #Clear board
+      pygame.draw.rect(windowSurface, White, (PingPong[0], PingPong[1], 4, 4)) #Ball
+
+      pygame.draw.rect(windowSurface, Player1CL, (Boundary, ((Height * (PlayerPosition[0] / 100))-20), 5, 40)) #Player One
+      pygame.draw.rect(windowSurface, Player2CL, (Width-Boundary-5, ((Height * (PlayerPosition[1] / 100))-20), 5, 40)) #Player Two
+      pygame.draw.rect(windowSurface, Player3CL, (((Width * (PlayerPosition[2] / 100))-20), 0, 40, 5)) #Player Three
+      pygame.draw.rect(windowSurface, Player4CL, (((Width * (PlayerPosition[3] / 100))-20), Height-5, 40, 5)) #Player Four
+      
       Render()
 
 pygame.quit()
