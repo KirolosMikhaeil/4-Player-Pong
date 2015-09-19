49f14667f877bebd0bf83891ba94efb538e50a3e
CoderEagel <sidney.bruneder854@outlook.com>
2015-09-19 12:50:16 +0930

    Fixed Spelling Mistakes

--- MacLinuxLauncher.sh
+++ MacLinuxLauncher.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-echo  Starting 4 Player P!ng without and arguments
 echo -----------------------------------------------
+echo |   Starting 4 Player P!ng without arguments  |
+echo -----------------------------------------------
+echo Launcher script by CoderEagel
 Python Main.py
